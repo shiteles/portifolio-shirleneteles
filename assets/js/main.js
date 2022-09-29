6de6c3695e2e91ac2d459f00555b5f657c7d41bd
Shirlene Teles <shiteles@gmail.com>
2022-09-29 17:19:02 -0300

    atualiza idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,8 +27,20 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.skills.softSkills')
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
 })()
